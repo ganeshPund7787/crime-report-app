de664712bd21994d102c4df2c328cb5452d8ab3f
ganeshPund7787 <ganupund7787@gmail.com>
2025-01-03 11:22:20 +0530

    minner changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,5 @@
 // //@typescript-eslint/ban-ts-comment
 // // @ts-ignore
-// import { PrismaClient } from "@prisma/client";
-
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// let prisma;
-
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-// export default prisma;
 
 import { PrismaClient } from "@prisma/client";
 
