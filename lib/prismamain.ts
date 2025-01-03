4c5b2a90d79174e4b36cab139b23b546aee87106
ganeshPund7787 <ganupund7787@gmail.com>
2025-01-03 11:44:31 +0530

    improve prisma file

--- lib/prismamain.ts
+++ lib/prismamain.ts
@@ -1,18 +1,20 @@
-// //@typescript-eslint/ban-ts-comment
-// // @ts-ignore
-
 import { PrismaClient } from "@prisma/client";
+
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+
 const prisma =
   global.prisma ||
   new PrismaClient({
     log:
-      process.env.NODE_ENV === "development" ? ["query", "info", "warn"] : [], // Optional logging in dev.
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : [], 
   });
+
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
